98a48450e0e679bcd52555647206ad52737602cf
Robert Cooper <rob.cooper@sellonlinedirect.com>
2022-01-12 14:19:14 +0000

    init commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>RaeTech Productions Ltd :: Aberdeen Based Videographers</title>
+
+        <link href="/css/output.css" rel="stylesheet">
+    </head>
+    <body class="antialiased w-full h-screen">
+        <div class="w-full h-full relative flex items-center justify-center min-h-screen bg-gwhite  py-4 text-center">
+            <div class="space-y-4">
+            <img src="images/logo.jpg" width="500" height="376" alt="RaeTech Products Ltd" >
+            <p>Temporary Holding Page - Full Website Coming Soon.</p>
+            <h3 class="text-xl font-bold">Contacts</h3>
+            <p><strong>Brian Rae:</strong> 01224 864646 <a href="mailto:brian@raetech.co.uk" class="underline">brian@raetech.co.uk</a></p>
+            <p><strong>Alan Rae:</strong> 01224 703745 <a href="mailto:alan@raetech.co.uk" class="underline">alan@raetech.co.uk</a></p>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['resources/views/**/*.php'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
